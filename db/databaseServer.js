d3db312f63508684557817b04592fecfa1d733b2
Johan Ossanes <johannossanes@gmail.com>
2025-06-15 21:07:07 -0300

    sv funcionando, get e post ok pra usuarios

--- db/databaseServer.js
+++ db/databaseServer.js
@@ -1,16 +1,37 @@
 import express from 'express'
 import cors from 'cors'
-import knexdb from './knexfile.js'
+import knex from 'knex'
+import knexconfig from './knexfile.js'
 
 const app = express()
-
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+
+const db = knex(knexconfig)
 
 app.get('/', (req, res) =>{
-    res.send('Servidor rodando!')
+    res.send('servidor ok')
 })
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000.")
+app.get('/usuarios', async (req, res) =>{
+    try{
+        const usuarios = await db('usuarios').select('*')
+        res.json(usuarios)
+    } catch (error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.post('/usuarios', async (req, res) =>{
+    try{
+        const {username, senha}= req.body
+        const [id]=await db('usuarios').insert({username, senha})
+        res.status(201).json({id, username, senha})
+    } catch (error){
+        res.status(500).json({error: error.message})
+    }
+})
+
+app.listen(3000, () =>{
+    console.log('servidor rodando na porta 3000 (!)')
 })
