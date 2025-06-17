14d4349521c3ea9c172cde3b3d51abd817961c61
jade <fernandes.l.jade@gmail.com>
2025-06-16 21:40:01 -0300

    login ok

--- view/index.js
+++ view/index.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', async () =>{
     const form = document.getElementById('login')
-    form.addEventListener('submit', (e)=>{
+    form.addEventListener('submit', async (e)=>{
         e.preventDefault()
 
         const nomeUsuarioValor = document.getElementById('nome-de-usuario').value.trim()
@@ -17,7 +17,32 @@ window.addEventListener('DOMContentLoaded', async () =>{
         }
 
         if (nomeUsuarioValor && senhaValor){
-            window.api.sendLoginSuccess()
+            try {
+                const response = await fetch('http://localhost:3000/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        username: nomeUsuarioValor,
+                        senha: senhaValor
+                    })
+                })
+
+                if (!response.ok){
+                    const errorData= await response.json()
+                    alert (errorData.error || 'Erro ao tentar login.')
+                    return
+                }
+
+                const dados = await response.json()
+                console.log("Login realizado com sucesso ", dados)
+                window.api.sendLoginSuccess()
+
+            } catch (error){
+                console.error("Erro na requisição:", error)
+                alert("Erro na conexão com o servidor.")
+            }
         }
     })
 })
